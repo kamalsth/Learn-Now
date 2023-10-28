9664e2e9cfca6b6890a58114e195950596c74dd0
Kamal Shrestha <kamal@lancemeup.com>
2023-10-28 17:18:24 +0545

    Backend Setup

---
+++ backend/src/main/java/com/learnnow/LearnnowApplication.java
@@ -0,0 +1,13 @@
+package com.learnnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnnowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/learnnow/LearnnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
